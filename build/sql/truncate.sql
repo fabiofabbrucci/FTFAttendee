e2620e88a48cf3152156ad02c4a891aae7364159
Fabio Fabbrucci <fabio.fabbrucci@gmail.com>
2012-06-18 11:07:38 +0200

    Initial commit

---
+++ build/sql/truncate.sql
@@ -0,0 +1,8 @@
+TRUNCATE TABLE abm_goal_tag;
+TRUNCATE TABLE abm_follow;
+TRUNCATE TABLE abm_notification;
+TRUNCATE TABLE abm_event;
+TRUNCATE TABLE abm_tag;
+TRUNCATE TABLE abm_cheer;
+TRUNCATE TABLE abm_goal;
+TRUNCATE TABLE abm_user;
